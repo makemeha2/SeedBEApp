9ea04f251c5bf22f6b962f1e6252068bfd0a572e
soyoung <makemeha2@gmail.com>
2025-09-29 13:53:54 +0900

    통합 테스트 완료

--- src/test/java/com/heyso/SeedBEApp/biz/board/web/BoardControllerIT.java
+++ src/test/java/com/heyso/SeedBEApp/biz/board/web/BoardControllerIT.java
@@ -14,9 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.nio.charset.StandardCharsets;
